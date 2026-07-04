643dc02f045ae4593a37d3b4115788e27513448b
Sergio <sebarren@ucm.es>
2026-07-04 14:26:18 +0200

    refactor: reorder round of 32 match sequence and implement admin dashboard frontend components

--- test_api.js
+++ test_api.js
@@ -176,22 +176,22 @@ function calculateUserBracket(predObj, dbMatches) {
   const userMatches = {};
   
   const r32Matches = [
-    { id: "M75", label: "D1", lRef: "1B", vRef: "3o Top 2" },
-    { id: "M76", label: "D2", lRef: "1C", vRef: "3o Top 3" },
-    { id: "M73", label: "D3", lRef: "1A", vRef: "3o Top 1" },
-    { id: "M77", label: "D4", lRef: "2C", vRef: "2D" },
-    { id: "M84", label: "D5", lRef: "1H", vRef: "3o Top 8" },
-    { id: "M83", label: "D6", lRef: "2G", vRef: "2H" },
-    { id: "M82", label: "D7", lRef: "1G", vRef: "3o Top 7" },
-    { id: "M81", label: "D8", lRef: "1F", vRef: "3o Top 6" },
-    { id: "M74", label: "D9", lRef: "2A", vRef: "2B" },
-    { id: "M78", label: "D10", lRef: "1D", vRef: "3o Top 4" },
-    { id: "M79", label: "D11", lRef: "1E", vRef: "3o Top 5" },
-    { id: "M80", label: "D12", lRef: "2E", vRef: "2F" },
-    { id: "M86", label: "D13", lRef: "1J", vRef: "2K" },
-    { id: "M85", label: "D14", lRef: "1I", vRef: "2J" },
-    { id: "M88", label: "D15", lRef: "1L", vRef: "2I" },
-    { id: "M87", label: "D16", lRef: "1K", vRef: "2L" }
+    { id: "M73", label: "D1", lRef: "1A", vRef: "3o Top 1" },
+    { id: "M74", label: "D2", lRef: "2A", vRef: "2B" },
+    { id: "M75", label: "D3", lRef: "1B", vRef: "3o Top 2" },
+    { id: "M76", label: "D4", lRef: "1C", vRef: "3o Top 3" },
+    { id: "M77", label: "D5", lRef: "2C", vRef: "2D" },
+    { id: "M78", label: "D6", lRef: "1D", vRef: "3o Top 4" },
+    { id: "M79", label: "D7", lRef: "1E", vRef: "3o Top 5" },
+    { id: "M80", label: "D8", lRef: "2E", vRef: "2F" },
+    { id: "M81", label: "D9", lRef: "1F", vRef: "3o Top 6" },
+    { id: "M82", label: "D10", lRef: "1G", vRef: "3o Top 7" },
+    { id: "M83", label: "D11", lRef: "2G", vRef: "2H" },
+    { id: "M84", label: "D12", lRef: "1H", vRef: "3o Top 8" },
+    { id: "M85", label: "D13", lRef: "1I", vRef: "2J" },
+    { id: "M86", label: "D14", lRef: "1J", vRef: "2K" },
+    { id: "M87", label: "D15", lRef: "1K", vRef: "2L" },
+    { id: "M88", label: "D16", lRef: "1L", vRef: "2I" }
   ];
   
   r32Matches.forEach(m => {
